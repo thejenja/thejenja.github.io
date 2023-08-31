5ad490e14cb7c170ee0905934b3e0577ef3604e2
Evgeniy Khramov <65224669+thejenja@users.noreply.github.com>
2023-08-31 23:00:16 +0500

    Big update (#8)
    * All new
    
    * Delete script.js
    
    * update

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].classList.remove("show");
+      tabcontent[i].classList.add("hide");
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].classList.remove("active");
+    }
+
+    document.getElementById(tabName).classList.remove("hide");
+    document.getElementById(tabName).classList.add("show");
+  
+    evt.currentTarget.classList.add("active");
+  }
+  
+  window.onload = function() {
+    document.getElementById("Tab1").classList.add("show");
+    document.getElementsByClassName("tablinks")[0].classList.add("active");  
+  };
